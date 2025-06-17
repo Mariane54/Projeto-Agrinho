f1485c2c00e4063a2c0e954a12e7237023a1937a
Mariane54 <137068595+Mariane54@users.noreply.github.com>
2025-06-17 14:14:35 +0000

    alteração

--- script.js
+++
@@ -1,9 +0,0 @@
-function participar() {
-  alert("Inscrições abertas em breve! Acompanhe nossas redes.");
-}
-
-document.getElementById("formContato").addEventListener("submit", function(e) {
-  e.preventDefault();
-  document.getElementById("msgEnviada").innerText = "Mensagem enviada com sucesso!";
-  this.reset();
-});
